555987b0d49fc133ef32b112d4472cc73910e60c
Keisuke Fukami <k-fukami@creationline.com>
2021-02-23 18:56:57 +0900

    fix yaml candidate error and script candidate error

--- package/scripts/run-develop-docker.sh
+++ package/scripts/run-develop-docker.sh
@@ -17,7 +17,7 @@ export arguments
 export ENABLE_DEBUGGER="${ENABLE_DEBUGGER:-false}"
 export DEBUGGER_PORT="${DEBUGGER_PORT:-8000}"
 if "${ENABLE_DEBUGGER}"; then
-  debugger_args="-Dspring-boot.run.jvmArguments=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=${DEBUGGER_PORT}"
+  debugger_args="-Dspring-boot.run.jvmArguments='-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=${DEBUGGER_PORT}'"
 else
   debugger_args=''
 fi
