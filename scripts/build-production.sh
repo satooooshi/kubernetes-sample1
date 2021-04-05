e7266323172bddceddf2ef2d4cf3f8f4b84cfed7
Keisuke Fukami <k-fukami@creationline.com>
2021-04-05 20:41:26 +0900

    add develop package/deployments

--- scripts/build-production.sh
+++ scripts/build-production.sh
@@ -4,10 +4,12 @@ set -eu
 # move to project root directory
 cd "`dirname $0`/../"
 
+export REPOSITORY="${REPOSITORY:+${REPOSITORY}/}"
+
 if [ "$#" -lt 1 ]; then
   echo "$0: There are no argument for component." 1>&2
   exit 1
 fi
 export component="$1"
 
-docker build -t "${component}:prod" -f "./package/${component}/production/Dockerfile" .
+docker build -t "${REPOSITORY}${component}:prod" -f "./package/${component}/production/Dockerfile" .
