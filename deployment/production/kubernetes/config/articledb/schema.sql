b712811d7ba86af81c29a4d0192bb09e57b001be
Keisuke Fukami <k-fukami@creationline.com>
2021-04-18 21:42:29 +0900

    update production manifests

--- deployment/production/kubernetes/config/articledb/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE article (
-    id bigserial NOT NULL
-    , title varchar NOT NULL
-    , author varchar NOT NULL
-    , body text NOT NULL
-    , CONSTRAINT article_id_pkc PRIMARY KEY (id)
-);
