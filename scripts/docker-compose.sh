78bc0a74a799e08fff6846d318bae8c7409f93a9
Keisuke Fukami <k-fukami@creationline.com>
2021-08-27 19:43:09 +0900

    update docker-compose.sh to use "docker compose" command if available

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -7,4 +7,13 @@ fi
 # move to project root directory
 cd "`dirname $0`/../"
 
-docker-compose -f "./deployment/develop/docker/docker-compose.yaml" "$@"
+if docker compose version 2>&1 > /dev/null; then
+  COMPOSE_COMMAND='docker compose'
+elif type docker-compose 2>&1 > /dev/null; then
+  COMPOSE_COMMAND='docker-compose'
+else
+  echo 'this script needs "compose" subcommand with "docker" cli, or "docker-compose".' 1>&2
+  exit 1
+fi
+
+${COMPOSE_COMMAND} -f "./deployment/develop/docker/docker-compose.yaml" "$@"
