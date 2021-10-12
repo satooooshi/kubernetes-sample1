ed74ca90a851e466bf6969051fa991873f5019dd
Keisuke Fukami <k-fukami@creationline.com>
2021-10-12 17:17:27 +0900

    add feature build and jaeger dependencies

---
+++ scripts/kaniko-build-feature.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+set -eu
+
+KANIKO_EXECUTOR='/kaniko/executor'
+if type executor 2>&1 > /dev/null; then
+  KANIKO_EXECUTOR=executor
+fi
+if ! ${KANIKO_EXECUTOR} version 2>&1 > /dev/null; then
+  echo 'this script need kaniko' 1>&2
+  exit 1
+fi
+
+# move to project root directory
+cd "`dirname $0`/../"
+
+export REPOSITORY="${REPOSITORY:+${REPOSITORY}/}"
+
+if [ "${BRANCH:-x}" = 'x' ]; then
+  echo "branch name unknown; use tag name ${tag:=feat}"
+else
+  echo "tag name specified from last path segment of branch name: ${tag:=${BRANCH##*/}}"
+fi
+export tag
+
+if [ "$#" -lt 1 ]; then
+  echo "$0: There are no argument for component." 1>&2
+  exit 1
+fi
+export component="$1"
+
+if [ "${REPOSITORY:-x}" = 'x' ]; then
+  destination_opt="--no-push"
+  cache_opt=""
+else
+  destination_opt="--destination=${REPOSITORY}${component}:${tag}"
+  cache_opt="--cache=true"
+fi
+
+"${KANIKO_EXECUTOR}" \
+  ${destination_opt} \
+  ${cache_opt} \
+  --dockerfile="${PWD}/package/${component}/develop/Dockerfile" \
+  --context="dir://${PWD}" \
